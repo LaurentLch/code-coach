111852eb00393c585fabe5851b428c1f193c0574
LaurentLch <laurentlonchay@gmail.com>
2021-05-11 16:28:21 +0200

    added service

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,14 +1,19 @@
 import {Component, OnInit} from '@angular/core';
+import {HelloWorldService} from "./hello-world.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'Not Yet Loaded';
 
+  constructor(private helloWorldService: HelloWorldService) {
+  }
+
   ngOnInit(): void {
-    this.title = 'Loaded';
+    this.title = this.helloWorldService.getHelloWorld();
+
   }
 }
