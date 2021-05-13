18ede98e1e3e342a22c42f8cf18869a1df1d37fc
Legion-exe <Legion-exe@gmail.com>
2021-05-13 18:23:10 +0200

    v

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import { RegistrationComponent } from './registration/registration.component';
-import {ReactiveFormsModule} from "@angular/forms";
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
