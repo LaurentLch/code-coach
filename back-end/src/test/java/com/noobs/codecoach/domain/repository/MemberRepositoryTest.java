f4b84f1d4f4c4899d5a6bd6b9839a1e7c6d58616
Legion-exe <Legion-exe@gmail.com>
2021-05-17 10:43:07 +0200

    Testing "RepositoryTest + Respository" modified

---
+++ back-end/src/test/java/com/noobs/codecoach/domain/repository/MemberRepositoryTest.java
@@ -0,0 +1,45 @@
+package com.noobs.codecoach.domain.repository;
+
+//import static org.junit.jupiter.api.Assertions.*;
+
+import com.noobs.codecoach.domain.entity.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class MemberRepositoryTest {
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    void memberCreate_ifMemberCreated_returnMembersCountPlusOne() {
+        long membersCount = memberRepository.count();
+        Member member3 = new Member("FNtest2", "LNtest2", "MAD2@gmail.com", "A2azerty");
+        memberRepository.save(member3);
+        long membersCountNew = memberRepository.count();
+        assertThat(membersCountNew).isEqualTo(membersCount + 1);
+    }
+
+    @Test
+    void createMember_ifMemberInDatabase_thenReturnInfos() {
+        Member member1 = new Member("FNtest1", "LNtest1", "MAD1@gmail.com", "A1azerty");
+        memberRepository.save(member1);
+        assertThat(member1.getFirstName()).isEqualTo("FNtest1");
+        assertThat(member1.getLastName()).isEqualTo("LNtest1");
+        assertThat(member1.getEmail()).isEqualTo("MAD1@gmail.com");
+        assertThat(member1.getPassword()).isEqualTo("A1azerty");
+    }
+
+    @Test
+    void findMemberByEmail_ifEmailInDatabase_thenReturnFirstName() {
+        Member member2 = new Member("FNtest2", "LNtest2", "MAD2@gmail.com", "A2azerty");
+        memberRepository.save(member2);
+        assertThat(memberRepository.getMemberByEmail("MAD2@gmail.com").getFirstName()).isEqualTo("FNtest2");
+    }
+
+}
