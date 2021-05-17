40fadf8d20b6413a148a5c00ca05a79001bde841
Frederik Collignon <frederik.collignon@minfin.fed.be>
2021-05-17 10:39:07 +0200

    Added MemberMapperTest

--- back-end/src/main/java/com/noobs/codecoach/service/dto/request/CreateMemberDTO.java
+++ back-end/src/main/java/com/noobs/codecoach/service/dto/request/CreateMemberDTO.java
@@ -1,12 +1,45 @@
 package com.noobs.codecoach.service.dto.request;
 
-import lombok.Getter;
-
-@Getter
 public class CreateMemberDTO {
 
     private String firstName;
     private String lastName;
     private String email;
     private String password;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public CreateMemberDTO setFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public CreateMemberDTO setLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public CreateMemberDTO setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public CreateMemberDTO setPassword(String password) {
+        this.password = password;
+        return this;
+    }
 }
