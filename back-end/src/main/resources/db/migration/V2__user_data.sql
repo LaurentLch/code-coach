787c10b09f5b1042bb375facf46b21b9d8d5613e
Frederik Collignon <frederik.collignon@minfin.fed.be>
2021-05-19 11:36:01 +0200

    Added autogenerated data for user and coach_topic

---
+++ back-end/src/main/resources/db/migration/V2__user_data.sql
@@ -0,0 +1,9 @@
+INSERT INTO users(id, "first_name", "last_name", "email", "password", "role")
+values ('1', 'Coachee', 'A', 'coachee@mail.be', 'P4sswordA', 'COACHEE');
+
+INSERT INTO users(id, "first_name", "last_name", "email", "password", "role")
+values ('2', 'Coach', 'B', 'coach@mail.be', 'P4sswordB', 'COACH');
+
+INSERT INTO users(id, "first_name", "last_name", "email", "password", "role")
+values ('3', 'Admin', 'C', 'admin@mail.be', 'P4sswordC', 'ADMINISTRATOR');
+
