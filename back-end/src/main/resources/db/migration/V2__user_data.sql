112577609f4ad85bcf318095c64ed25b0cddd576
Frederik Collignon <frederik.collignon@minfin.fed.be>
2021-05-19 13:33:19 +0200

    Fixed data

--- back-end/src/main/resources/db/migration/V2__user_data.sql
+++ back-end/src/main/resources/db/migration/V2__user_data.sql
@@ -1,9 +1,9 @@
-INSERT INTO users(id, "first_name", "last_name", "email", "password", "role")
-values ('1', 'Coachee', 'A', 'coachee@mail.be', 'P4sswordA', 'COACHEE');
+INSERT INTO users("first_name", "last_name", "email", "password", "role")
+values ('Coachee', 'A', 'coachee@mail.be', 'P4sswordA', 'COACHEE');
 
-INSERT INTO users(id, "first_name", "last_name", "email", "password", "role")
-values ('2', 'Coach', 'B', 'coach@mail.be', 'P4sswordB', 'COACH');
+INSERT INTO users("first_name", "last_name", "email", "password", "role")
+values ('Coach', 'B', 'coach@mail.be', 'P4sswordB', 'COACH');
 
-INSERT INTO users(id, "first_name", "last_name", "email", "password", "role")
-values ('3', 'Admin', 'C', 'admin@mail.be', 'P4sswordC', 'ADMINISTRATOR');
+INSERT INTO users("first_name", "last_name", "email", "password", "role")
+values ('Admin', 'C', 'admin@mail.be', 'P4sswordC', 'ADMINISTRATOR');
 
