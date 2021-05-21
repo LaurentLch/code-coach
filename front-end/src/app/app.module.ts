6354e8c0465dff593fb35c9d4580523191531e26
LaurentLch <laurentlonchay@gmail.com>
2021-05-21 11:25:07 +0200

    deployment compiling issues fixed

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,16 +1,16 @@
-// @ts-ignore
+
 import { NgModule } from '@angular/core';
-// @ts-ignore
+
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// @ts-ignore
+
 import {HttpClientModule} from '@angular/common/http';
 import { RegistrationComponent } from './components/user/registration/registration.component';
-// @ts-ignore
+
 import {ReactiveFormsModule} from '@angular/forms';
-// @ts-ignore
+
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './components/navigation/header/header.component';
 import { FooterComponent } from './components/navigation/footer/footer.component';
@@ -22,8 +22,6 @@ import { CoachSessionsComponent } from './components/user/profile/coach-sessions
 import { FindCoachComponent } from './components/user/profile/find-coach/find-coach.component';
 
 
-
-// @ts-ignore
 @NgModule({
   declarations: [
     AppComponent,
@@ -35,9 +33,7 @@ import { FindCoachComponent } from './components/user/profile/find-coach/find-co
     BecomeCoachComponent,
     HomeComponent,
     CoachSessionsComponent,
-    FindCoachComponent
-
-
+    FindCoachComponent,
   ],
   imports: [
     BrowserModule,
