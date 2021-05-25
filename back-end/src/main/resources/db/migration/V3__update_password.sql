701318cf32a5ec86e199ccabcdeac5de33070295
Legion-exe <Legion-exe@gmail.com>
2021-05-25 16:34:59 +0200

    TestingOnUserAccount-SQL

--- back-end/src/main/resources/db/migration/V3__update_password.sql
+++ back-end/src/main/resources/db/migration/V3__update_password.sql
@@ -1,3 +1,22 @@
 update users
 set password = '$2y$12$ebtU3oVIUj1M1BFk1eE1Kekb1hlKHX7XKnhrq5Yz2bUwyBJlRR0mG'
 where email = 'frederik.collignon@hotmail.com';
+
+update users
+set password = ''
+where email = 'coachee1@codecoach.org';
+update users
+set password = '$2y$12$ebtU3oVIUj1M1BFk1eE1Kekb1hlKHX7XKnhrq5Yz2bUwyBJlRR0mG'
+where email = 'coachee2@codecoach.org';
+update users
+set password = '$2y$12$ebtU3oVIUj1M1BFk1eE1Kekb1hlKHX7XKnhrq5Yz2bUwyBJlRR0mG'
+where email = 'coach1@codecoach.org';
+update users
+set password = '$2y$12$ebtU3oVIUj1M1BFk1eE1Kekb1hlKHX7XKnhrq5Yz2bUwyBJlRR0mG'
+where email = 'coach2@codecoach.org';
+update users
+set password = '$2y$12$ebtU3oVIUj1M1BFk1eE1Kekb1hlKHX7XKnhrq5Yz2bUwyBJlRR0mG'
+where email = 'admin1@codecoach.org';
+update users
+set password = '$2y$12$ebtU3oVIUj1M1BFk1eE1Kekb1hlKHX7XKnhrq5Yz2bUwyBJlRR0mG'
+where email = 'admin2@codecoach.org';
