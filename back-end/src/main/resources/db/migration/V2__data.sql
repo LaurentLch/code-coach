09cf29e762e3a9099667586d0335ac2c9f901f2e
Legion-exe <Legion-exe@gmail.com>
2021-05-26 10:17:31 +0200

    Correct FlyAway - Version

--- back-end/src/main/resources/db/migration/V2__data.sql
+++ back-end/src/main/resources/db/migration/V2__data.sql
@@ -9,24 +9,4 @@ INSERT INTO coach_topic(id, experience_level, "topic_name", fk_coach_info_id)
 values ('3', '5', 'Topic3', '1');
 
 INSERT INTO users(id, "email", "first_name", "last_name", "password", "role", coach_info_id)
-values ('1', 'frederik.collignon@hotmail.com', 'Frederik', 'Collignon', 'P4ssword', 'Coachee', '1');
-
-INSERT INTO users(id, "email", "first_name", "last_name", "password", "role", coach_info_id)
-values ('2', 'coachee1@codecoach.org', 'Coachee1', 'testCoachee1', 'YouC0ach', 'Coachee', '1');
-
-INSERT INTO users(id, "email", "first_name", "last_name", "password", "role", coach_info_id)
-values ('3', 'coachee2@codecoach.org', 'Coachee2', 'testCoachee2', 'YouC0ach', 'Coache', '1');
-
-INSERT INTO users(id, "email", "first_name", "last_name", "password", "role", coach_info_id)
-values ('4', 'coach1@codecoach.org', 'Coach1', 'testCoach1', 'YouC0ach', 'Coach', '1');
-
-INSERT INTO users(id, "email", "first_name", "last_name", "password", "role", coach_info_id)
-values ('5', 'coach2@codecoach.org', 'Coach2', 'testCoach2', 'YouC0ach', 'Coach', '1');
-
-INSERT INTO users(id, "email", "first_name", "last_name", "password", "role", coach_info_id)
-values ('6', 'admin1@codecoach.org', 'Admin1', 'testAdmin1', 'YouC0ach', 'Admin', '1');
-
-INSERT INTO users(id, "email", "first_name", "last_name", "password", "role", coach_info_id)
-values ('7', 'admin2@codecoach.org', 'Admin2', 'testAdmin2', 'YouC0ach', 'Admin', '1');
-
---coachee	           coachee1@codecoach.org	                  YouC0ach
+values ('1', 'frederik.collignon@hotmail.com', 'Frederik', 'Collignon', 'P4ssword', 'Coach', '1');
