0b943a9c7023c5fca4d2827fbfcdeaf2a0d5489c
Legion-exe <Legion-exe@gmail.com>
2021-05-28 14:05:47 +0200

    FlyAway - GenericUsers

---
+++ back-end/src/main/resources/db/migration/V4__generic-users-data.sql
@@ -0,0 +1,12 @@
+INSERT INTO users(id, "email", "first_name", "last_name", "password", "role", coach_info_id)
+values ('2', 'coachee1@gmail.com', 'coachee1', 'coachee1lastname', 'P4ssword', 'Coachee', '1');
+INSERT INTO users(id, "email", "first_name", "last_name", "password", "role", coach_info_id)
+values ('3', 'coachee2@gmail.com', 'coachee2', 'coachee2lastname', 'P4ssword', 'Coachee', '1');
+INSERT INTO users(id, "email", "first_name", "last_name", "password", "role", coach_info_id)
+values ('4', 'coach1@gmail.com', 'coach1', 'coach1lastname', 'P4ssword', 'Coach', '1');
+INSERT INTO users(id, "email", "first_name", "last_name", "password", "role", coach_info_id)
+values ('5', 'coach2@gmail.com', 'coach2', 'coach2lastname', 'P4ssword', 'Coach', '1');
+INSERT INTO users(id, "email", "first_name", "last_name", "password", "role", coach_info_id)
+values ('6', 'admin1@gmail.com', 'admin1', 'admin1lastname', 'P4ssword', 'Admin', '1');
+INSERT INTO users(id, "email", "first_name", "last_name", "password", "role", coach_info_id)
+values ('7', 'admin2@gmail.com', 'admin2', 'admin2lastname', 'P4ssword', 'Admin', '1');
---
+++ back-end/src/main/resources/db/migration/V5__update-password-generic-users.sql
@@ -0,0 +1,18 @@
+update users
+set password = '$2y$12$ebtU3oVIUj1M1BFk1eE1Kekb1hlKHX7XKnhrq5Yz2bUwyBJlRR0mG'
+where email = 'coachee1@gmail.com';
+update users
+set password = '$2y$12$ebtU3oVIUj1M1BFk1eE1Kekb1hlKHX7XKnhrq5Yz2bUwyBJlRR0mG'
+where email = 'coachee2@gmail.com';
+update users
+set password = '$2y$12$ebtU3oVIUj1M1BFk1eE1Kekb1hlKHX7XKnhrq5Yz2bUwyBJlRR0mG'
+where email = 'coach1@gmail.com';
+update users
+set password = '$2y$12$ebtU3oVIUj1M1BFk1eE1Kekb1hlKHX7XKnhrq5Yz2bUwyBJlRR0mG'
+where email = 'coach2@gmail.com';
+update users
+set password = '$2y$12$ebtU3oVIUj1M1BFk1eE1Kekb1hlKHX7XKnhrq5Yz2bUwyBJlRR0mG'
+where email = 'admin1@gmail.com';
+update users
+set password = '$2y$12$ebtU3oVIUj1M1BFk1eE1Kekb1hlKHX7XKnhrq5Yz2bUwyBJlRR0mG'
+where email = 'admin2@gmail.com';
