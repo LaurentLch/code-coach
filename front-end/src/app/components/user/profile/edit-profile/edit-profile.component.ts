54a83da0d2ff29f8342e52f4de3737065cc45970
Frederik Collignon <frederik.collignon@minfin.fed.be>
2021-06-06 22:24:17 +0200

    Personal details added with non functional edit button

--- front-end/src/app/components/user/profile/edit-profile/edit-profile.component.ts
+++ front-end/src/app/components/user/profile/edit-profile/edit-profile.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {User} from '../../../../model/user';
+import {UserService} from '../../../../service/user.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-edit-profile',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditProfileComponent implements OnInit {
 
-  constructor() { }
+  user: User | undefined;
+  id: number | undefined;
+
+  constructor(private userService: UserService, private route: ActivatedRoute) {
+    // @ts-ignore
+    this.route.parent.paramMap.subscribe(params => {
+      // @ts-ignore
+      this.id = params.get('id');
+    });
+  }
 
   ngOnInit(): void {
+    this.getUser();
+  }
+
+  getUser(): void {
+    // @ts-ignore
+    this.userService.getUser(this.id).subscribe(user => this.user = user);
   }
 
 }
